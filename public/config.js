0ba1af71af7d7086cd8b332f442e68dede1ff5c6
Neel <Neel@DESKTOP-REC10H8>
2026-03-21 00:18:42 +0530

    messae

--- public/config.js
+++ public/config.js
@@ -2,4 +2,4 @@
 // Change this URL to your Render backend URL after deploying
 // Example: window.BACKEND_URL = 'https://bingo-backend-xxxx.onrender.com';
 // Leave empty string for local development (same origin)
-window.BACKEND_URL = 'https://bingo-backend-qcam.onrender.com';
+window.BACKEND_URL = '';
